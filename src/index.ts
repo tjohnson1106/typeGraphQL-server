e9164fc00b490f761b99b640224eab1440063704
Thomas Johnson <tjohnson1106@gmail.com>
2019-01-11 15:15:45 -0500

    created initial server configuration and test resolver

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import "reflect-metadata";
+import { ApolloServer } from "apollo-server-express";
+import * as express from "express";
+import { buildSchema, Resolver, Query } from "type-graphql";
+
+// test resolver
+@Resolver()
+class HelloResolver {
+  @Query(() => String, {
+    name: "HelloWorldAdjust"
+  })
+  async hello() {
+    return await "Hello World";
+  }
+}
+
+const main = async () => {
+  const schema = await buildSchema({
+    resolvers: [HelloResolver]
+  });
+
+  const apolloServer = new ApolloServer({ schema });
+
+  const app = express();
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("Server started on localhost:4000/graphql");
+  });
+};
+
+main();
