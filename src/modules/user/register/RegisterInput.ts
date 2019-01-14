7cd812ed362771a95e59163e1f35a8c2b92d1008
Thomas Johnson <tjohnson1106@gmail.com>
2019-01-14 13:57:02 -0500

    created register input fields

---
+++ src/modules/user/register/RegisterInput.ts
@@ -0,0 +1,17 @@
+import { MaxLength, Length } from "class-validator";
+import { InputType, Field } from "type-graphql";
+
+@InputType()
+export class RegisterInput {
+  @Field()
+  firstName: string;
+
+  @Field()
+  lastName: string;
+
+  @Field()
+  email: string;
+
+  @Field()
+  password: string;
+}
