36faa355a2fd306c365c60884f50f40706712585
Thomas Johnson <tjohnson1106@gmail.com>
2019-01-14 14:08:53 -0500

    added class validation to register input

--- src/modules/user/register/RegisterInput.ts
+++ src/modules/user/register/RegisterInput.ts
@@ -1,15 +1,18 @@
-import { MaxLength, Length } from "class-validator";
+import { Length, IsEmail } from "class-validator";
 import { InputType, Field } from "type-graphql";
 
 @InputType()
 export class RegisterInput {
   @Field()
+  @Length(1, 255)
   firstName: string;
 
   @Field()
+  @Length(1, 255)
   lastName: string;
 
   @Field()
+  @IsEmail()
   email: string;
 
   @Field()
