46c4d3c0db522a13a570a5add0257ba731e7e809
Thomas Johnson <tjohnson1106@gmail.com>
2019-01-14 15:45:32 -0500

    created login resolver and created my context type

---
+++ src/modules/user/Login.ts
@@ -0,0 +1,36 @@
+import { Resolver, Mutation, Arg, Ctx } from "type-graphql";
+import bcrypt from "bcryptjs";
+
+import { User } from "../../entity/User";
+import { MyContext } from "../../types/MyContext";
+
+@Resolver()
+export class LoginResolver {
+  // login returns User | null
+  @Mutation(() => User, { nullable: true })
+  async login(
+    @Arg("email") email: string,
+    @Arg("password") password: string,
+    @Ctx() ctx: MyContext
+  ): Promise<User | null> {
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      return null;
+    }
+
+    // compare passwords
+
+    const valid = await bcrypt.compare(password, user.password);
+
+    if (!valid) {
+      return null;
+    }
+
+    // return cookie with req object via ctx decorator
+
+    ctx.req.session!.userId = user.id;
+
+    return user;
+  }
+}
