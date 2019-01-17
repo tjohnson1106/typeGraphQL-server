b78ef11520cc33ad9eb04788b47640970a54ab6f
Thomas Johnson <tjohnson1106@gmail.com>
2019-01-17 13:15:02 -0500

    confirmation url import redis prefix fix

--- src/modules/utils/createConfirmationUrl.ts
+++ src/modules/utils/createConfirmationUrl.ts
@@ -1,12 +1,12 @@
 import { redis } from "../../redis";
 import { v4 } from "uuid";
-import { confirmationPrefix } from "../constants/redisPrefixes";
+import { confirmUserPrefix } from "../constants/redisPrefixes";
 
 export const createConfirmationUrl = async (userId: number) => {
   // send token to server to verify
   // token should be stored in Redis
   const token = v4();
-  await redis.set(confirmationPrefix + token, userId, "ex", 60 * 60 * 24); // 1 Day
+  await redis.set(confirmUserPrefix + token, userId, "ex", 60 * 60 * 24); // 1 Day
 
   // redirect
   return `http://localhost:3000/user/confirm/${token}`;
