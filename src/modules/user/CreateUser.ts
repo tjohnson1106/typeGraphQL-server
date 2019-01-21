809e5845f86f56c82c5c0eadc124f79b94adbdec
Thomas Johnson <tjohnson1106@gmail.com>
2019-01-21 14:42:34 -0500

    created create user mutation

---
+++ src/modules/user/CreateUser.ts
@@ -0,0 +1,14 @@
+import { Resolver, Mutation, Arg } from "type-graphql";
+import { RegisterInput } from "./register/RegisterInput";
+import { User } from "../../entity/User";
+
+@Resolver()
+export class CreateUserResolver {
+  @Mutation(() => User)
+  async createUser(
+    @Arg("data")
+    data: RegisterInput
+  ) {
+    return User.create(data).save();
+  }
+}
